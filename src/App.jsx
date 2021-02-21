81e5dc51cd07792ee545ac940b73023bafabb64e
jhon esteban <jhon9901211@gmail.com>
2021-02-20 22:07:40 -0500

    usando template literals, add de funcion del button eraser

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import React, {useState} from 'react';
-import Functions from './components/Functions';
-import Numbers from './components/Numbers';
-import MathOperations from './components/MathOperations';
-import Result from './components/Result';
-import './App.css';
+    import React, {useState} from 'react';
+    import Functions from './components/Functions';
+    import Numbers from './components/Numbers';
+    import MathOperations from './components/MathOperations';
+    import Result from './components/Result';
+    import './App.css';
 
-// Función Flecha o Arrow Function
-const App = () => {
+    // Función Flecha o Arrow Function
+    const App = () => {
 
     //Array Destructuring
     
@@ -16,28 +16,36 @@ const App = () => {
     const [stack, setStack] = useState ("")    
     
     // Lo que ejecuta la función
-    console.log("Renderización de App")
+    console.log("Renderización de App");
     return (
-    <main className='react-calculator'>
-    <Result value={stack} />
-    <Numbers onClickNumber={number => {
-        console.log("Click en number", number)
-        setStack(number)
-    }}/>
+        <main className='react-calculator'>
+        <Result value={stack} />
+        <Numbers onClickNumber={number => {
+            console.log("Click en number", number);
+            setStack(`${stack}${number}`);
+        }}/>
         
         <Functions 
-            onContentClear={() => 
-                console.log("Content Clear")}
-            onDelete={() => 
-                console.log("onDelete")}
+            onContentClear={() => {
+                console.log("Content Clear");
+                setStack('');
+            }}
+
+            onDelete={() => {
+                const newStack = stack.substring(0, stack.length -1);
+                console.log("onDelete");
+                setStack(newStack);
+            }}
         />
         <MathOperations 
-            onClickOperation={operation => 
-                console.log("Operation:", operation)
-            } 
-            onClickEqual={equal => 
-                console.log("Equal:", equal)
-            }
+            onClickOperation={operation => {
+                console.log("Operation:", operation);
+                setStack(`${stack}${operation}`);
+            }}
+            onClickEqual={equal => {
+                    console.log("Equal:", equal);
+                    setStack(`${stack}${equal}`);
+            }}
         />
         </main>
     )
